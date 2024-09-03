d67e504b95579214d88e96701189366688d7047c
Lancerokobe <141099012+Lancerokobe@users.noreply.github.com>
2024-09-03 15:14:41 +0000

    Printed Book1,2,3

--- Book.java
+++ Book.java
@@ -1,1 +1,18 @@
+public class Book {
+    private String title;
+    private String author;
+    private int yearPublished;
+    private double price;
+    public Book(String title, String author, int yearPublished, double price) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.price = price;
+    }
+    public String toString() {
+        return "Title: \"" + title + "\"\nAuthor: \"" + author + 
+               "\"\nYear Published: " + yearPublished + 
+               "\nPrice: $" + price;
+    }
+}
 
